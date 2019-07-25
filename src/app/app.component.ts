d9476e77280ddf1bbc36bacf435e1c2868050340
Willem Hustinx <willemhustinx@gmail.com>
2019-07-25 16:02:28 +0200

    Navigation and Routing Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: ` 
-  <div><h1>{{pageTitle}}</h1>
-    <pm-products></pm-products>
+  template: `
+  <nav class='nav navbar-expand navbar-light bg-light'>
+    <a class='navbar-brand'>{{pageTitle}}</a>
+    <ul class='nav nav-pills'>
+      <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+      <li><a class='nav-link' [routerLink]="['/products']">Product</a></li>
+    </ul>
+  </nav>
+  <div class='container'>
+    <router-outlet></router-outlet>
   </div>
   `
   //templateUrl: './app.component.html',
